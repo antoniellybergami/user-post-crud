97d22ff4e9baade74f3c92c6255edf7fbc38f011
antoniellybergami <antoniellybergami@gmail.com>
2025-01-17 12:38:06 -0300

    Criação do CRUD POSTs + Relacionamento com user

---
+++ src/main/kotlin/com/example/crud_usuarios/model/Post.kt
@@ -0,0 +1,24 @@
+package com.example.crud_usuarios.model
+
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotEmpty
+
+@Entity
+data class Post (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id : Long = 0,
+
+    @field:NotEmpty(message = "title cannot be empty")
+    @Column(nullable = false)
+    var title : String,
+
+    @field:NotEmpty(message = "description cannot be empty")
+    @Column(columnDefinition = "text")
+    var description : String,
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "userId")
+    var user : Usuario,
+)
+
